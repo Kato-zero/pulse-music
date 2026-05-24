4a6c960e4b607f228092e5370ca3f025d4d0e709
Kato-zero <catochibala@gmail.com>
2026-05-24 12:18:21 +0200

    Refactor service worker caching logic and variables

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,30 @@
-const CACHE_NAME = "pulse-music-v1";
+const CACHE = "pulse-v1";
 
-const FILES = [
+const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./service-worker.js",
   "./icons/icon-192.png"
 ];
 
-self.addEventListener("install", (e) => {
+self.addEventListener("install", e=>{
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
+    caches.open(CACHE).then(c=>c.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (e) => {
+self.addEventListener("activate", e=>{
   e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k!==CACHE && caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener("fetch", e=>{
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then(r=>r||fetch(e.request))
   );
 });
